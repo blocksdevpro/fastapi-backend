ef7e04d3eae784970c4bb3db889cd6f06bd5ce49
BlocksDevPro <blocksdev.pro@gmail.com>
2025-12-20 14:11:00 +0530

    feat(tests): initialized pytest and implemented async test cases for /auth

---
+++ tests/test_auth.py
@@ -0,0 +1,41 @@
+from httpx import AsyncClient, Response
+import pytest
+
+from app.schemas.auth import AuthResponse, SignupRequest, LoginRequest
+from app.schemas.response import APIResponse
+
+
+user_credentials = {"name": "Uttam", "email": "mail@uttam.com", "password": "Pass!123"}
+
+auth_response: APIResponse[AuthResponse]
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_health(client: AsyncClient):
+    res = await client.get("/health")
+    assert res.status_code == 200
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_signup(client: AsyncClient):
+    res: Response = await client.post(
+        "/auth/signup",
+        json=SignupRequest(**user_credentials).model_dump(),
+    )
+    assert res.status_code == 200
+    assert res.json() != {}
+    global auth_response
+    auth_response = APIResponse[AuthResponse](**res.json())
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_login(client: AsyncClient):
+    res = await client.post("/auth/login",
+                      json=LoginRequest(**user_credentials).model_dump())
+    assert res.status_code == 200
+    assert res.json() != {}
+    
+    global auth_response
+    auth_response = APIResponse[AuthResponse](**res.json())
+    
+
+
