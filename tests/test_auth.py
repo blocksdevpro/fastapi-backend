0a291e2ee8cb8904fbaa367835d87b566403138c
BlocksDevPro <blocksdev.pro@gmail.com>
2025-12-22 22:03:14 +0530

    feat(tests): add more test cases which does the faliure check for /auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -17,7 +17,7 @@ async def test_health(client: AsyncClient):
 
 
 @pytest.mark.asyncio(loop_scope="session")
-async def test_signup(client: AsyncClient):
+async def test_success_signup(client: AsyncClient):
     res: Response = await client.post(
         "/auth/signup",
         json=SignupRequest(**user_credentials).model_dump(),
@@ -27,15 +27,41 @@ async def test_signup(client: AsyncClient):
     global auth_response
     auth_response = APIResponse[AuthResponse](**res.json())
 
+
 @pytest.mark.asyncio(loop_scope="session")
-async def test_login(client: AsyncClient):
-    res = await client.post("/auth/login",
-                      json=LoginRequest(**user_credentials).model_dump())
+async def test_failed_signup(client: AsyncClient):
+    res: Response = await client.post(
+        "/auth/signup",
+        json=SignupRequest(**user_credentials).model_dump(),
+    )
+    assert res.status_code == 400
+    res_json = res.json()
+    assert res_json != {}
+    assert not res_json["success"]
+    assert res_json["error"]["message"] == "User already exists"
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_success_login(client: AsyncClient):
+    res = await client.post(
+        "/auth/login", json=LoginRequest(**user_credentials).model_dump()
+    )
     assert res.status_code == 200
     assert res.json() != {}
-    
+
     global auth_response
     auth_response = APIResponse[AuthResponse](**res.json())
-    
 
 
+@pytest.mark.asyncio(loop_scope="session")
+async def test_failed_login(client: AsyncClient):
+    res = await client.post(
+        "/auth/login",
+        json=LoginRequest(email=user_credentials["email"], password="123").model_dump(),
+    )
+    assert res.status_code == 401
+    res_json = res.json()
+
+    assert res_json != {}
+    assert not res_json["success"]
+    assert res_json["error"]["message"] == "Invalid credentials"
